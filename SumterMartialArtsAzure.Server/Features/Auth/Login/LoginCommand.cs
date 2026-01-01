7c0af49d1f5da4594184befe0a5123afca949952
CheesePizza100 <whaley8991@gmail.com>
2026-01-01 16:11:30 -0500

    Use password and not email.

--- SumterMartialArtsAzure.Server/Features/Auth/Login/LoginCommand.cs
+++ SumterMartialArtsAzure.Server/Features/Auth/Login/LoginCommand.cs
@@ -2,7 +2,7 @@
 
 namespace SumterMartialArtsAzure.Server.Api.Features.Auth.Login;
 
-public record LoginRequest(string UserName, string Email);
+public record LoginRequest(string UserName, string Password);
 
 public record LoginCommand(string Username, string Password)
     : IRequest<LoginCommandResponse>;
