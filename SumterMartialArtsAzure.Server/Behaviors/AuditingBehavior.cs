a69380b272d59734a3f3256e2a09d54987c151ac
CheesePizza100 <whaley8991@gmail.com>
2026-01-02 06:41:12 -0500

    Add AuditingBehavior and LoginAuditingBehavior. Add registrations.

---
+++ SumterMartialArtsAzure.Server/Behaviors/AuditingBehavior.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using SumterMartialArtsAzure.Server.Api.Auditing;
+using SumterMartialArtsAzure.Server.Services;
+
+namespace SumterMartialArtsAzure.Server.Api.Behaviors;
+
+public class AuditingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>, IAuditableCommand
+    where TResponse : IAuditableResponse
+{
+    private readonly IAuditService _auditService;
+    private readonly ICurrentUserService _currentUserService;
+
+    public AuditingBehavior(IAuditService auditService, ICurrentUserService currentUserService)
+    {
+        _auditService = auditService;
+        _currentUserService = currentUserService;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (!_currentUserService.IsAuthenticated())
+        {
+            return await next();
+        }
+
+        var response = await next();
+
+        await _auditService.LogAsync(
+            request.Action,
+            request.EntityType,
+            response.EntityId,
+            response.GetAuditDetails()
+        );
+
+        return response;
+    }
+}
---
+++ SumterMartialArtsAzure.Server/Behaviors/LoginAuditingBehavior.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using SumterMartialArtsAzure.Server.Api.Features.Auth.Login;
+using SumterMartialArtsAzure.Server.DataAccess;
+using SumterMartialArtsAzure.Server.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumterMartialArtsAzure.Server.Api.Behaviors;
+
+public class LoginAuditingBehavior : IPipelineBehavior<LoginCommand, LoginCommandResponse>
+{
+    private readonly AppDbContext _dbContext;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public LoginAuditingBehavior(AppDbContext dbContext, IHttpContextAccessor httpContextAccessor)
+    {
+        _dbContext = dbContext;
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public async Task<LoginCommandResponse> Handle(
+        LoginCommand request,
+        RequestHandlerDelegate<LoginCommandResponse> next,
+        CancellationToken cancellationToken)
+    {
+        var response = await next();
+
+        var auditLog = AuditLog.Create(
+            response.UserId,
+            response.Username,
+            AuditActions.UserLoggedIn,
+            "User",
+            response.UserId.ToString(),
+            _httpContextAccessor.HttpContext?.Connection.RemoteIpAddress?.ToString() ?? "unknown"
+        );
+
+        _dbContext.AuditLogs.Add(auditLog);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return response;
+    }
+}
