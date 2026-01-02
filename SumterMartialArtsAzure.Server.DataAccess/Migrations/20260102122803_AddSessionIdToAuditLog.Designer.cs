0985d52a21ee43a52c01556445af4983daee3b10
CheesePizza100 <whaley8991@gmail.com>
2026-01-02 07:36:15 -0500

    Add SessionId migration.

--- SumterMartialArtsAzure.Server.DataAccess/Migrations/20260102122803_AddSessionIdToAuditLog.Designer.cs
+++ SumterMartialArtsAzure.Server.DataAccess/Migrations/20260102122803_AddSessionIdToAuditLog.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SumterMartialArtsAzure.Server.DataAccess;
 
@@ -11,9 +12,11 @@ using SumterMartialArtsAzure.Server.DataAccess;
 namespace SumterMartialArtsAzure.Server.DataAccess.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260102122803_AddSessionIdToAuditLog")]
+    partial class AddSessionIdToAuditLog
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
